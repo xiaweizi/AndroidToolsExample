ec92875d77820b8bf18870e1799cda87ff437379
xiaweizi <1012126908@qq.com>
2018-01-31 21:04:38 +0800

    [dev] 添加 CommonActivity

--- app/src/main/java/com/xiaweizi/androidtoolsexample/CommonActivity.java
+++ app/src/main/java/com/xiaweizi/androidtoolsexample/CommonActivity.java
@@ -2,17 +2,12 @@ package com.xiaweizi.androidtoolsexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CommonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void common(View view) {
-
+        setContentView(R.layout.activity_common);
     }
 }
